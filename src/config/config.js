6e3ddd3866d0db9eabb0abbb4fdb984b00d82f0e
OreLana <lana.oreoluwa2@gmail.com>
2024-11-04 09:30:56 -0700

    connect db to github

--- src/config/config.js
+++ src/config/config.js
@@ -11,11 +11,12 @@ const config = {
     dialect: process.env.DB_DIALECT || 'mysql',
   },
   test: {
-    username: process.env.DB_USER_TEST || 'root',
-    password: process.env.DB_PASSWORD_TEST || null,
-    database: process.env.DB_NAME_TEST || 'database_test',
-    host: process.env.DB_HOST_TEST || '127.0.0.1',
+    username: process.env.DB_USERNAME || 'test_user',
+    password: process.env.DB_PASSWORD || 'test_password',
+    database: process.env.DB_DATABASE || 'test_db',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false,
   },
   production: {
     username: process.env.DB_USER_PROD,
