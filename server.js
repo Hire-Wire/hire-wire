4e310633995139e45d047386f345fc4f5740375d
OreLana <lana.oreoluwa2@gmail.com>
2024-11-18 23:51:24 -0700

    update cors policy

--- server.js
+++ server.js
@@ -16,8 +16,15 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // Middleware Setup
+const allowedOrigins = process.env.CORS_ORIGIN?.split(',') || ['http://localhost:8000'];
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:8000',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 }));
